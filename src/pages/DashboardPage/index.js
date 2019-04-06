e62200ba8529f9d128c7c3fec37684bacc6bc222
Low Jun Chen Marx <marxlow94@gmail.com>
2019-04-06 17:42:35 +0800

    Add in care taker view

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -3,6 +3,7 @@ import AppHeader from 'shared/layouts/AppHeader';
 import { Tabs } from 'antd';
 import PetSection from './components/PetSection';
 import CareTakerSearchSection from './components/CareTakerSearchSection';
+import CareTakerView from './CareTakerView';
 
 const TabPane = Tabs.TabPane;
 
@@ -12,12 +13,13 @@ class DashboardPage extends Component {
     this.state = {
       pets: [],
       userId: localStorage.getItem('userId'), // Read userId from localStorage for making subsequent requests
+      role: localStorage.getItem('role'),
     }
   }
 
   onLogout = ((e) => {
     e.preventDefault();
-    localStorage.removeItem('userId'); // User is no longer logged in
+    localStorage.clear(); // Remove all key/value pair in localstorage
     this.props.history.push('/login');
   });
 
@@ -27,7 +29,7 @@ class DashboardPage extends Component {
   });
 
   render() {
-    console.log('> logged in user is:', this.state.userId);
+    const { userId, role } = this.state;
     return (
       <div>
         <AppHeader onLogout={this.onLogout} onSearch={this.onSearch} />
@@ -37,7 +39,7 @@ class DashboardPage extends Component {
             <div className="col-sm-4 col-lg-3">
               {/* Profile Section */}
               <div className="card border-0">
-                <img className="card-img-top rounded-circle" src="http://placehold.it/200x200" alt="profile" />
+                <img className="card-img-top rounded-circle" src="https://www.freeiconspng.com/uploads/jake-the-dog-cartoon-characters-adventure-time-png--18.png" alt="profile" />
               </div>
               <div className="d-flex flex-column align-items-center mt-4">
                 <h4>Bob</h4>
@@ -45,27 +47,30 @@ class DashboardPage extends Component {
               </div>
             </div>
             <div className="col-8">
-              {/* Tabs */}
-              <Tabs type="card">
-                {/* Setting pet information */}
-                <TabPane tab="Profile" key="1">
-                  <PetSection pets={this.state.pets}/>
-                </TabPane>
+              {role === 'owner' ?
+                < Tabs type="card">
+                  {/* Setting pet information */}
+                  <TabPane tab="Profile" key="1">
+                    <PetSection pets={this.state.pets} />
+                  </TabPane>
 
-                {/* Setting dates that each dog wants to be taken care of */}
-                {/* <TabPane tab="Dates" key="2">
-                  <DateSection />
+                  {/* Setting dates that each dog wants to be taken care of */}
+                  {/* <TabPane tab="Dates" key="2">
+                  <DateSection title="Select a date range for all your pets to be taken care of"/>
                 </TabPane> */}
 
-                {/* Searching & Bidding for care takers */}
-                <TabPane tab="Search" key="3">
-                  <CareTakerSearchSection />
-                </TabPane>
-              </Tabs>
+                  {/* Searching & Bidding for care takers */}
+                  <TabPane tab="Search" key="3">
+                    <CareTakerSearchSection />
+                  </TabPane>
+                </Tabs>
+                :
+                <CareTakerView />
+              }
             </div>
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
